44d966dd5a06e28b5828b9c60a045e7175274a61
Shoun <shoun.pho@gmail.com>
2018-04-05 09:55:46 +0200

    bug fix

--- app/src/main/java/com/pjtrans/pecunia/MainActivity.java
+++ app/src/main/java/com/pjtrans/pecunia/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
