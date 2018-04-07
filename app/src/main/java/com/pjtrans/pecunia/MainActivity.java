0c12737b519f798396c78a6e030a332cfd8ebac6
Shoun <shoun.pho@gmail.com>
2018-04-07 23:48:26 +0200

    permissions automatically asked

--- app/src/main/java/com/pjtrans/pecunia/MainActivity.java
+++ app/src/main/java/com/pjtrans/pecunia/MainActivity.java
@@ -1,19 +1,31 @@
 package com.pjtrans.pecunia;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
     public static final int REQUEST_IMAGE_CAPTURE = 11;
+    public static final int MULTIPLE_PERMISSIONS = 22;
+
+    String[] permissions= new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
+
     Button mTake_Pic;
     Button mSelect_Pic;
 
@@ -22,8 +34,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTake_Pic = (Button) findViewById(R.id.activity_main_take_picture);
-        mSelect_Pic = (Button) findViewById(R.id.activity_main_select_picture);
+        if(checkPermissions());
+
+        mTake_Pic = findViewById(R.id.activity_main_take_picture);
+        mSelect_Pic = findViewById(R.id.activity_main_select_picture);
 
         mTake_Pic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +55,35 @@ public class MainActivity extends Activity {
                 //user clicked
             }
         });
+    }
+
+    private  boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p:permissions) {
+            result = ContextCompat.checkSelfPermission(this,p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),MULTIPLE_PERMISSIONS );
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permissions granted.
+                } else {
+
+                }
+                // permissions list of don't granted permission
+            }
+        }
     }
 }
